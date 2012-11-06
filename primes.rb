64a4fe03f0819b5431b08be91396fa362a9bbcd8
ElaineTai <lalalainexd@gmail.com>
2012-11-05 22:43:44 -0500

    renames the boolean methods to have question masets private methods

--- primes.rb
+++ primes.rb
@@ -6,9 +6,8 @@ class Problem7
 
     curr_number = 2
     while @primes.size < n
-      unless can_factor curr_number
+      unless can_factor? curr_number
         @primes.add curr_number
-
       end
       curr_number += 1
     end
@@ -17,18 +16,28 @@ class Problem7
     
   end
 
-  def can_factor(n)
-   can_factor_by_primes(n) || can_factor_by_single_digits(n)
+  def can_factor?(n)
+    ( not is_single_digit_prime?(n) ) && ( can_factor_by_single_digits?(n) || can_factor_by_primes?(n) )
   end
 
-  def can_factor_by_primes(n)
+  def can_factor_by_primes?(n)
     @primes.inject(false){|contains, i| contains || n % i == 0 }
   end
 
-  def can_factor_by_single_digits(n)
+  #Any number not a prime can be factored by some single digit
+  def can_factor_by_single_digits?(n)
     single_digits = (2..9).to_a  
     single_digits.inject(false){|contains, i| contains || n % i == 0 }
   end
+
+  def is_single_digit_prime?(n)
+    n == 2 || n == 3 || n == 5 || n == 7
+  end
+
+  private :can_factor?
+  private :can_factor_by_primes?
+  private :can_factor_by_single_digits?
+  private :is_single_digit_prime?
   
 end
 
