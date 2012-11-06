2142434dd8dc5b8c962287a3720167cec9b667ef
ElaineTai <lalalainexd@gmail.com>
2012-11-06 00:12:28 -0500

    adds normal implementation to compare with optimized implementation and adds tests for normal implementation and a test that prints out a timing comparison for both implementations

--- primes.rb
+++ primes.rb
@@ -1,6 +1,21 @@
 require 'set'
 
 class Problem7
+  def find_nth_prime_optimized(n)
+    @primes = SortedSet.new 
+
+    curr_number = 2
+    while @primes.size < n
+      unless can_factor_optimized? curr_number
+        @primes.add curr_number
+      end
+      curr_number += 1
+    end
+
+    return @primes.to_a.last
+    
+  end
+  
   def find_nth_prime(n)
     @primes = SortedSet.new 
 
@@ -15,8 +30,15 @@ class Problem7
     return @primes.to_a.last
     
   end
+  
+  private
 
   def can_factor?(n)
+    n != 2 && can_factor_by_primes?(n)
+  end
+
+  #Any number not a prime can be factored by some single digit or a prime number
+  def can_factor_optimized?(n)
     ( not is_single_digit_prime?(n) ) && ( can_factor_by_single_digits?(n) || can_factor_by_primes?(n) )
   end
 
@@ -24,7 +46,6 @@ class Problem7
     @primes.inject(false){|contains, i| contains || n % i == 0 }
   end
 
-  #Any number not a prime can be factored by some single digit
   def can_factor_by_single_digits?(n)
     single_digits = (2..9).to_a  
     single_digits.inject(false){|contains, i| contains || n % i == 0 }
@@ -34,15 +55,4 @@ class Problem7
     n == 2 || n == 3 || n == 5 || n == 7
   end
 
-  private :can_factor?
-  private :can_factor_by_primes?
-  private :can_factor_by_single_digits?
-  private :is_single_digit_prime?
-  
 end
-
-prob = Problem7.new
-
-result = prob.find_nth_prime 1001
-
-puts result
--- primes_test.rb
+++ primes_test.rb
@@ -1,18 +1,63 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'time'
 require_relative 'primes'
 
 describe Problem7 do
-  it "should return 2 when finding the first prime" do
-    Problem7.new.find_nth_prime(1).must_equal 2 
+  describe "Normal implementation" do
+    it "should return 2 when finding the first prime" do
+      Problem7.new.find_nth_prime(1).must_equal 2 
+    end
+
+    it "should return 3 when finding the second prime" do
+      Problem7.new.find_nth_prime(2).must_equal 3 
+    end
+
+    it "should return 11 when finding the 5th prime" do
+      Problem7.new.find_nth_prime(5).must_equal 11 
+    end
+
+    it "should return 229 when finding the 50th prime" do
+      Problem7.new.find_nth_prime(50).must_equal 229 
+    end
+
   end
-  it "should return 3 when finding the second prime" do
-    Problem7.new.find_nth_prime(2).must_equal 3 
+
+  describe "Optimized implementation" do
+    it "should return 2 when finding the first prime" do
+      Problem7.new.find_nth_prime_optimized(1).must_equal 2 
+    end
+
+    it "should return 3 when finding the second prime" do
+      Problem7.new.find_nth_prime_optimized(2).must_equal 3 
+    end
+
+    it "should return 11 when finding the 5th prime" do
+      Problem7.new.find_nth_prime_optimized(5).must_equal 11 
+    end
+
+    it "should return 229 when finding the 50th prime" do
+      Problem7.new.find_nth_prime_optimized(50).must_equal 229 
+    end
+
   end
-  it "should return 11 when finding the 5th prime" do
-    Problem7.new.find_nth_prime(5).must_equal 11 
+
+  describe "Compare optimized and normal implementation" do
+    it "should return 7927 when finding the 1001st prime" do
+      start_time = Time.now
+      Problem7.new.find_nth_prime(1001).must_equal 7927 
+      normal_time_elapsed = Time.now - start_time 
+       
+
+      start_time = Time.now
+      Problem7.new.find_nth_prime_optimized(1001).must_equal 7927 
+      optimized_time_elapsed = Time.now - start_time
+
+      optimized_time_elapsed.must_be :< , normal_time_elapsed
+
+      puts "Normal implementation takes #{normal_time_elapsed} to find the 1001st prime."
+      puts "Optimized implementation takes #{optimized_time_elapsed} to find the 1001st prime."
+    end
   end
-  it "should return 229 when finding the 50th prime" do
-    Problem7.new.find_nth_prime(50).must_equal 229 
-  end
+
 end
