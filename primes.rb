2e03f5f967e9042a0ba72d66d42683cca25776bb
Franklin Webber <franklin.webber@gmail.com>
2012-12-05 17:34:20 -0500

    Minor Optimization and changes for clarity
    * Memoizing the single_digits and single_digit_primes
    * Starting the set off with the number 2 and removing the check for 2

--- primes.rb
+++ primes.rb
@@ -2,9 +2,9 @@ require 'set'
 
 class Problem7
   def find_nth_prime_optimized(n)
-    @primes = SortedSet.new 
+    @primes = initial_set_of_primes
 
-    curr_number = 2
+    curr_number = 3
     while @primes.size < n
       unless can_factor_optimized? curr_number
         @primes.add curr_number
@@ -12,14 +12,13 @@ class Problem7
       curr_number += 1
     end
 
-    return @primes.to_a.last
-    
+    @primes.to_a.last
   end
-  
-  def find_nth_prime(n)
-    @primes = SortedSet.new 
 
-    curr_number = 2
+  def find_nth_prime(n)
+    @primes = initial_set_of_primes
+
+    curr_number = 3
     while @primes.size < n
       unless can_factor? curr_number
         @primes.add curr_number
@@ -27,14 +26,13 @@ class Problem7
       curr_number += 1
     end
 
-    return @primes.to_a.last
-    
+    @primes.to_a.last
   end
-  
+
   private
 
   def can_factor?(n)
-    n != 2 && can_factor_by_primes?(n)
+    can_factor_by_primes?(n)
   end
 
   #Any number not a prime can be factored by some single digit or a prime number
@@ -47,12 +45,23 @@ class Problem7
   end
 
   def can_factor_by_single_digits?(n)
-    single_digits = (2..9).to_a  
     single_digits.inject(false){|contains, i| contains || n % i == 0 }
   end
 
   def is_single_digit_prime?(n)
-    n == 2 || n == 3 || n == 5 || n == 7
+    single_digit_primes.include?(n)
+  end
+
+  def initial_set_of_primes
+    SortedSet.new([2])
+  end
+
+  def single_digits
+    @single_digits ||= (2..9).to_a
+  end
+
+  def single_digit_primes
+    @single_digit_primes ||= [ 2, 3, 5, 7 ]
   end
 
 end
